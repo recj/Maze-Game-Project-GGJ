875e4796ffdeaf3e2f6ada9e1cf30757600e950a
Eber <52986565+doneber@users.noreply.github.com>
2022-01-31 16:49:16 -0400

    Agrega Scena Menu de inicoi

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//usamos esta libreria
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void BotonStart()
+    {
+        SceneManager.LoadScene("OnGame");//para cargar la escena
+    }
+
+    // Update is called once per frame
+    public void BotonQuit()
+    {
+        Debug.Log("Gracias por jugar amigx <3");
+        Application.Quit();//esta parte solo funcionara en el juego construido por la build,en el editor no da
+    }
+}
